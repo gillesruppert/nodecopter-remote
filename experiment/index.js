f433b0ea8dd5019aa6a229c7cd99c67cd4088303
Gilles Ruppert <gilles@madeofbytes.com>
2013-04-11 00:04:24 +0200

    hook up video and stream to the browser

--- experiment/index.js
+++ experiment/index.js
@@ -5,10 +5,3 @@ var dronestream = require('dronestream');
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-  fs.createReadStream(__dirname + '/index.html').pipe(res);
-
-});
-
-dronestream.listen(server);
-server.listen(5555);
